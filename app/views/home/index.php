03245b3110340e120c9bdadb7db6692a853c800f
hermawanarby <hermawanbaru4@gmail.com>
2021-10-01 16:56:12 +0700

    Merapihkan jumbotron

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -2,8 +2,10 @@
 <div class="p-5 mb-4 bg-light rounded-3 mt-4">
   <div class="container-fluid py-5">
     <h1 class="display-5 fw-normal">Semalat Datang di Website Saya!</h1>
-    <p class="col-md-8 fs-6">Halo, nama saya <?= $data['nama']; ?></p>
-    <button class="btn btn-primary btn-lg" type="button">Example button</button>
+    <p class="col-md-8 fs-5">Halo, nama saya <?= $data['nama']; ?></p>
+    <hr class="my-4">
+    <p class="col-md-8 fs-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga itaque porro ut necessitatibus.</p>
+    <button class="btn btn-primary btn-lg" type="button">Learn more</button>
   </div>
 </div>
 </div>
