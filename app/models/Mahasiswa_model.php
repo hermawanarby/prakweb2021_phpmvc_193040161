feadcd1960835cda8aa37e2d0abd7447936f4a04
hermawanarby <hermawanbaru4@gmail.com>
2021-10-01 17:30:42 +0700

    Memperbaharui dari model data mahasiswa menggunakan array menjadi mengugunakan database

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -1,31 +1,26 @@
 <?php 
 
 class Mahasiswa_model {
-  private $mhs = [
-    [
-      "nama" => "Hermawan Arby",
-      "nrp" => "193040161",
-      "email" => "hermawan@gmail.com",
-      "jurusan" => "Teknik Informatika"
-    ],
-    [
-      "nama" => "Rendi Jualiansyah",
-      "nrp" => "193020100",
-      "email" => "rendi@gmail.com",
-      "jurusan" => "Teknik Mesin"
-    ],
-    [
-      "nama" => "Ahmad Sahroni",
-      "nrp" => "194030123",
-      "email" => "ahmad@gmail.com",
-      "jurusan" => "Teknik Industri"
-    ]
-  ];
+  private $dbh; // database handler
+  private $stmt;
+
+  public function __construct()
+  {
+    // data source name
+    $dsn = 'mysql:host=localhost; dbname=phpmvc_193040161';
+
+    try {
+      $this->dbh = new PDO($dsn, 'root', '');
+    } catch (PDOException $e) {
+      die($e->getMessage());
+    }
+  }
 
   public function getAllMahasiswa() 
   {
-    return $this->mhs;
+    $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
+    $this->stmt->execute();
+    return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
   }
-
 
 }
