fc70fea5b5eade689504362f47ef27bc68542cee
hermawanarby <hermawanbaru4@gmail.com>
2021-10-01 14:59:13 +0700

    Menambah parameter umur di method index About.php

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,9 +1,9 @@
 <?php 
 
 class About {
-  public function index($nama = 'Hermawan', $pekerjaan = 'Mahasiswa') 
+  public function index($nama = 'Hermawan', $pekerjaan = 'Mahasiswa', $umur = 20) 
   {
-    echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan";
+    echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan. Saya berumur $umur tahun.";
   }
 
   public function page() 
